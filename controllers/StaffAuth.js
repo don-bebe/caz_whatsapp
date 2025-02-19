2653a5b2bb085b8f85951e56dfea9f92c5b9af48
don-bebe <wabantubebe@gmail.com>
2025-02-19 10:07:47 +0200

    user login

--- controllers/StaffAuth.js
+++ controllers/StaffAuth.js
@@ -132,6 +132,7 @@ const allStaff = async (req, res) => {
           required: false,
           order: [["lastLogin", "DESC"]],
           limit: 1,
+          separate: true,
         },
       ],
     });
